d5a7c4787c6b3dd3477853d8e7a3ea89a815d8ba
Tasha-Tech <tasha.tech04@gmail.com>
2021-12-15 15:22:39 -0500

    Created SQL submit function

--- NestFinder/README.md
+++ NestFinder/README.md
@@ -14,5 +14,9 @@ beautifulsoup4
 sqlalchemy
 
 
+## Binder link
+https://hub.gke2.mybinder.org/user/tasha-tech-nestfinder-eaykzfl4/lab/tree/NestFinder
 
+## Rebuild binder with
+https://mybinder.org/v2/gh/Tasha-Tech/NestFinder/832686997078c8d3ffb1e6f1892987b68ce0008c?urlpath=lab%2Ftree%2FNestFinder%2FTutorial.ipynb
 
--- NestFinder/finder.py
+++ NestFinder/finder.py
@@ -9,37 +9,46 @@ import on_sale
 def meters_to_miles(meters):
     return meters * 0.000621371
 
+class Finder:
+    def __init__(self):
+        s = schools.load('downloads')
+        # a = address.load('downloads')
 
-def test():
-    s = schools.load('downloads')
-    # a = address.load('downloads')
+        #  https://www.w3.org/2015/spatial/wiki/Coordinate_Reference_Systems
+        #  Distance may be invalid for a geographic CRS using degrees as units
+        #  Using GeoSeries.to_crs() to project geometries to a planar CRS before using geopandas.GeoSeries.distance
+        if 'Geographic' in s.crs.type_name:
+            s = s.to_crs(3857)
 
-    #  https://www.w3.org/2015/spatial/wiki/Coordinate_Reference_Systems
-    #  Distance may be invalid for a geographic CRS using degrees as units
-    #  Using GeoSeries.to_crs() to project geometries to a planar CRS before using geopandas.GeoSeries.distance
-    if 'Geographic' in s.crs.type_name:
-        s = s.to_crs(3857)
+        houses = on_sale.from_disk('downloads').to_crs(3857)
 
-    houses = on_sale.from_disk('downloads').to_crs(3857)
+        print(s['TYPE'].unique())
+        for school_type in s['TYPE'].unique():
+            st = s[s['TYPE'] == school_type]
+            # Very interesting article about performance of row iterations
+            # https://towardsdatascience.com/apply-function-to-pandas-dataframe-rows-76df74165ee4
+            houses[school_type + '_school'] = pd.Series(
+                [meters_to_miles(st.distance(p).min()) for p in houses.geometry])
 
-    print(s['TYPE'].unique())
-    for school_type in s['TYPE'].unique():
-        st = s[s['TYPE'] == school_type]
-        # Very interesting article about performance of row iterations
-        # https://towardsdatascience.com/apply-function-to-pandas-dataframe-rows-76df74165ee4
-        houses[school_type+'_school'] = pd.Series([meters_to_miles(st.distance(p).min()) for p in houses.geometry])
+        print(list(houses.columns))
 
-    print(list(houses.columns))
+        # Create an in-memory SQLite database.
+        self.sql_engine = create_engine('sqlite://', echo=False)
+        sql_columns = ['address', 'listPrice', 'lotSize', 'yearBuilt']
+        [sql_columns.append(col + '_school') for col in s['TYPE'].unique()]
+        houses[sql_columns].to_sql('on_sale', con=self.sql_engine)
 
-    # Create an in-memory SQLite database.
-    sql_engine = create_engine('sqlite://', echo=False)
-    sql_columns = ['address', 'listPrice', 'lotSize', 'yearBuilt']
-    [sql_columns.append(col+'_school') for col in s['TYPE'].unique()]
-    houses[sql_columns].to_sql('on_sale', con=sql_engine)
-    report = sql_engine.execute("SELECT address, listPrice, Middle_school FROM on_sale WHERE Middle_school < 0.4").fetchall()
-    print(*report, sep='\n')
-    print(f'Found {len(report)} matching records')
+    def submit(self, stmt: str):
+        return self.sql_engine.execute(stmt).fetchall()
+
+
+finder = Finder()
+
+
+def submit(stmt: str):
+    return finder.submit(stmt)
 
 
 if __name__ == "__main__":
-    test()
+    res = submit("SELECT address, listPrice, Middle_school FROM on_sale WHERE Middle_school < 0.4")
+    print(*res, sep='\n')
